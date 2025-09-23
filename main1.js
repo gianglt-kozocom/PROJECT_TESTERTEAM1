1e302d92ad4566f224d8895e5ba64519e2d25f4f
gianglt-kozocom <gianglt@kozo-japan.com>
2025-09-23 15:58:32 +0700

    Update main1 & main2

--- main1.js
+++ main1.js
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-//giang update
-=======
 
->>>>>>> origin/main
 let car = {
   brand: "Force",
   model: "Teriyaki",
   year: 2025,
-<<<<<<< HEAD
   color: "black",
   getAge() {
     let currentYear = new Date().getFullYear();
@@ -26,28 +21,4 @@ console.log(car.getInfo());
 console.log("Age", car.getAge());
 console.log(car.paint("red"));
 console.log(car.getInfo());
-=======
-  color: "black"
-};
 
-// Function: tính tuổi xe
-function getAge(car) {
-  let currentYear = new Date().getFullYear();
-  return currentYear - car.year;
-}
-
-// Function: mô tả xe
-function getInfo(car) {
-  return `${car.brand} ${car.model}, year ${car.year}, color ${car.color}`;
-}
-
-// Function: đổi màu xe
-function paint(car, newColor) {
-  car.color = newColor;
-  return `New color is ${car.color}`;
-}
-console.log(getInfo(car));         
-console.log("Age", getAge(car)); 
-console.log(paint(car, "red"));    
-console.log(getInfo(car));         
->>>>>>> origin/main
